78632f5a07b763d2d07d25da371311128c0f9d7c
windx-foobar <bezalkogoln1ycoder@gmail.com>
2022-04-17 21:37:41 +0300

    фейковый логгер для тестов

--- test-helpers/FakeLogger.ts
+++ test-helpers/FakeLogger.ts
@@ -1,10 +1,10 @@
 import { FakeLogger as Logger } from '@adonisjs/logger/build';
 
 export class FakeLogger extends Logger {
-  constructor() {
+  constructor(level: 'info' | 'debug' = 'debug') {
     super({
       name: 'fake',
-      level: 'debug',
+      level,
       enabled: true
     });
 
@@ -12,13 +12,22 @@ export class FakeLogger extends Logger {
     const errorMethods: string[] = ['error'];
 
     for (const method of logMethods) {
-      this[method] = function(...args: any[]) {
-        console.log(...args);
+      this[method] = function(this: FakeLogger, ...args: any[]) {
+        if (method === 'debug') {
+          if (this.level === 'debug') {
+            console.log(`LOGGER ${method.toUpperCase()}`);
+            console.log(...args);
+          }
+        } else {
+          console.log(`LOGGER ${method.toUpperCase()}`);
+          console.log(...args);
+        }
       }.bind(this);
     }
 
     for (const method of errorMethods) {
-      this[method] = function(...args: any[]) {
+      this[method] = function(this: FakeLogger, ...args: any[]) {
+        console.log(`LOGGER ${method.toUpperCase()}`);
         console.error(...args);
       }.bind(this);
     }
